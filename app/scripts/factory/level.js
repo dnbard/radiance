db91993cb8c1decc9adfe379236195b625d6972a
Alex Bardanov <dnbard@gmail.com>
2015-01-07 15:04:24 +0200

    transfered level extensions to separate module

--- app/scripts/factory/level.js
+++ app/scripts/factory/level.js
@@ -1,66 +1,8 @@
 define([
-    'lodash',
     'core/factory',
-    'generators/level',
-    'core/objects',
-    'config/general'
-], function(_, factory, generators, Objects, config){
-    factory.registerMethod('addTile', function(data){
-        if (!this.tiles){
-            this.tiles = {};
-        }
 
-        this.tiles[this.getTileLocation(data.gridX, data.gridY)] = data.id;
-    });
-
-    factory.registerMethod('getTile', function(x, y){
-        var tileId;
-
-        if (!this.tiles){
-            this.tiles = {};
-        }
-
-        tileId = this.tiles[this.getTileLocation(x, y)];
-
-        if (!tileId){
-            return null;
-        }
-
-        return Objects.get(tileId);
-    });
-
-    factory.registerMethod('getTileLocation', function(x, y){
-        return x + ',' + y;
-    });
-
-    factory.registerMethod('generate', function(data){
-        var generator = generators[this.generator];
-
-        if (!generator){
-            throw new Error('Generator ' + this.generator + ' not found');
-        }
-
-        _.bind(generator, this)(data);
-    });
-
-    factory.registerMethod('getTilePosition', function(){
-        var tile;
-
-        if (arguments.length === 1 && typeof arguments[0] === 'object'){
-            tile = arguments[0];
-
-            return {
-                x: tile.x,
-                y: tile.y
-            };
-        } else if (arguments.length === 2){
-            return {
-                x: arguments[0] * config.tileWidth,
-                y: arguments[1] * config.tileHeight
-            }
-        }
-    });
-
+    'factory/extenders/level'
+], function(factory){
     return factory.create({
         name: 'level',
         methods: ['addTile', 'getTile', 'getTileLocation', 'getTilePosition', 'generate'],
