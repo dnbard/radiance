6d75c9a99cfa8e093a7111a780ce3a5110faec02
Alex Bardanov <dnbard@gmail.com>
2015-01-10 18:20:58 +0200

    create sprite layers with correspondence with their z-index

--- app/scripts/helpers/spriteGroup.js
+++ app/scripts/helpers/spriteGroup.js
@@ -5,15 +5,16 @@ define([
 ], function(_, enums, phaser){
     var config = [{
             type: enums.Sprites.DOODAD,
-            z: 10
+            z: 5
         },{
             type: enums.Sprites.FLOOR,
             z: 1
         },{
             type: enums.Sprites.CREATURE,
-            z: 50
+            z: 10
         }],
-        groups = {};
+        groups = {},
+        initialized = false;
 
     /*groups[enums.Sprites.DOODAD] = _.first(phaser.GAMES).add.group(undefined, enums.Sprites.DOODAD);
     groups[enums.Sprites.FLOOR] = _.first(phaser.GAMES).add.group(undefined, enums.Sprites.FLOOR);*/
@@ -37,11 +38,21 @@ define([
 
     return {
         get: function(groupId){
+            if (!initialized){
+                _.chain(config).sortBy(function(configEntity){
+                    return configEntity.z;
+                }).each(function(configEntity){
+                    console.log(configEntity.type);
+                    tryInitGroup(configEntity.type);
+                });
+                initialized = true;
+            }
+
             if (!groupId){
                 throw new Error('Invalid argument: groupId');
             }
 
-            if (!groups[groupId] && !tryInitGroup(groupId)){
+            if (!groups[groupId]){
                 throw new Error('No predefined group for ' + groupId);
             }
 
